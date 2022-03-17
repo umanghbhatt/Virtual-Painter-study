448fbcae7235f977eada8c0d57ab0d92e42d9451
Bhavesh202021 <78691810+Bhavesh202021@users.noreply.github.com>
2022-03-17 18:20:41 +0530

    change Port settings

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def live():
 def liveCount():
     return render_template('countFinger.html')
 if __name__ == "__main__":
-    app.run(debug = True ,port = 5000)
+    app.run(debug = False ,port = 5000)
